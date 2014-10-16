eeb75f79e0385d903af242df14c8769b1ec5e3d8
Matt Keef <mkeef1@gmail.com>
2014-10-16 16:05:36 -0500

    gyroscope working

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -6,12 +6,19 @@
     $scope.ball = {};
 
     $scope.reset = function(){
-      $scope.ball.x = parseFloat(Math.random());
-      $scope.ball.y = parseFloat(Math.random());
+      $scope.ball.x = parseFloat((Math.random()) * 20).toFixed();
+      $scope.ball.y = parseFloat((Math.random()) * 20).toFixed();
+      $scope.ball = {x: $scope.ball.x, y: $scope.ball.y};
       console.log('ballPosxre>', $scope.ball.x);
       console.log('ballPosyre>', $scope.ball.y);
-      $scope.ball = {};
     };
+
+    window.addEventListener('deviceorientation', function(data){
+      $scope.data = data;
+      $scope.$digest();
+    });
+
+
     console.log('ball>', $scope.ball);
     console.log('ballPosx>', $scope.ball.x);
     console.log('ballPosy>', $scope.ball.y);
